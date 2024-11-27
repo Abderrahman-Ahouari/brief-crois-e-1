534dac9bc69892d843d846696b1c5b098b0d88e4
Abderrahmane_Ahouari <abdorrahmane054@gmail.com>
2024-11-27 12:26:13 +0100

    j'ai fini la creataion de cart module responsable d'afficher les information des joueur et jai creer la formulaire pour ajouté un joueur avec tous les input nessecaire j'ai pas encore valider les input, j'ai apporter la nessessaire data des joueur en un fichier json et je vais commencer sa consomation maintenant

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+async function loadPlayers() {
+    try {
+      const response = await fetch('../assets/js/players.json');
+      const data = await response.json();
+      const playerList = document.getElementById('playerList');
+        console.log(data)
+
+      data.players.forEach(player => {
+        const card = document.createElement('div');
+        card.className = 'player-card';
+        card.innerText = player.name;
+        card.draggable = true; // Pour drag-and-drop
+        playerList.appendChild(card);
+      });
+    } catch (error) {
+      console.error('Erreur lors du chargement des joueurs :', error);
+    }
+  } 
+
+  // Appeler la fonction pour charger les joueurs
+  loadPlayers();
+  
